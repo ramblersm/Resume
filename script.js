46fbd2e59d6b5b4ad57c7a744a2df4f3f62b982e
Soumya Mukherjee <contactsoumyam@gmail.com>
2023-08-21 22:00:35 +0530

    Initial file commits
    Initial file commits aug 21

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("myModal");
+    const openModalBtn = document.getElementById("openModal");
+    const closeModalBtn = document.getElementById("closeModal");
+  
+    openModalBtn.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+  
+    closeModalBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  
+    // Automatically open the modal when the page loads
+    modal.style.display = "block";
+  });
+  
