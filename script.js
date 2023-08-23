5cead693610bfa22d764b57cc01b7766c33a3e7a
Soumya Mukherjee <contactsoumyam@gmail.com>
2023-08-23 15:20:57 +0530

    fixed close button issue, added GA Tag

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const modal = document.getElementById("myModal");
     const openModalBtn = document.getElementById("openModal");
     const closeModalBtn = document.getElementById("closeModal");
+    const resumeIframe = document.getElementById("resumeIframe");
   
     openModalBtn.addEventListener("click", () => {
       modal.style.display = "block";
     });
   
     closeModalBtn.addEventListener("click", () => {
+      resumeIframe.src = ""; // Clear the iframe source
       modal.style.display = "none";
     });
   
     // Automatically open the modal when the page loads
-    modal.style.display = "block";
+    //modal.style.display = "block";
   });
   
